7aef6a920387d33b799c3db60e03447970baeea7
jacobsvennevik <87362637+jacobsvennevik@users.noreply.github.com>
2023-08-25 15:06:09 +0200

    Links, fungerer ikke NoPage

---
+++ frontend_react/src/Pages/NoPage/NoPage.jsx
@@ -0,0 +1,13 @@
+//NoPage.js
+import React from 'react';
+
+const NoPage = () => {
+  return (
+    <div style={{ padding: 20 }}>
+    <h2>404: Page Not Found</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+  </div>
+  );
+};
+
+export default NoPage;
